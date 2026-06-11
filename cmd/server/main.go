5fa2fa950597ba97e95a9e70d66bafe79c2015b3
Denis Lukhmanov <denis.lukhmanov@brainsome.com>
2026-06-11 19:03:54 +0300

    Iter 6: Add logging middleware which store all data about request

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -31,13 +31,9 @@ func main() {
 	r := chi.NewRouter()
 	storage := repository.NewMemStorage()
 	metricsService := service.NewMetricsService(storage)
-	updateHandlerConcrete := update_handler.NewHandler(metricsService)
-	getValueHandlerConcrete := get_value.NewHandler(metricsService)
-	getValueListHandlerConcrete := get_value_list.NewHandler(metricsService)
-
-	r.Get("/", getValueListHandlerConcrete.ServeHTTP)
-	r.Get("/value/{metricType}/{metricName}", getValueHandlerConcrete.ServeHTTP)
-	r.Post("/update/{metricType}/{metricName}/{metricValue}", updateHandlerConcrete.ServeHTTP)
+	updateH := update_handler.NewHandler(metricsService)
+	getValueH := get_value.NewHandler(metricsService)
+	getValueListH := get_value_list.NewHandler(metricsService)
 
 	logger, err := zap.NewDevelopment()
 	if err != nil {
@@ -45,7 +41,13 @@ func main() {
 	}
 	defer logger.Sync()
 
-	sugar := *logger.Sugar()
+	sugar := logger.Sugar()
+
+	r.Use(withLogging(sugar))
+
+	r.Get("/", getValueListH.ServeHTTP)
+	r.Get("/value/{metricType}/{metricName}", getValueH.ServeHTTP)
+	r.Post("/update/{metricType}/{metricName}/{metricValue}", updateH.ServeHTTP)
 
 	srv := &http.Server{
 		Addr:         addr,
@@ -57,9 +59,9 @@ func main() {
 
 	// Graceful start
 	go func() {
-		sugar.Infow("starting server", zap.String("addr", addr))
+		sugar.Infow("starting server", "addr", addr)
 		if err := srv.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
-			sugar.Infow("server closed", zap.Error(err))
+			sugar.Infow("server closed", "err", err)
 			os.Exit(1)
 		}
 	}()
@@ -75,9 +77,61 @@ func main() {
 	defer cancel()
 
 	if err := srv.Shutdown(ctx); err != nil {
-		sugar.Errorw("server forced to shutdown", zap.Error(err))
+		sugar.Errorw("server forced to shutdown", "err", err)
 		os.Exit(1)
 	}
 
 	sugar.Infow("server stopped gracefully")
+}
+
+type (
+	responseData struct {
+		status int
+		size   int
+	}
+
+	loggingResponseWriter struct {
+		http.ResponseWriter
+		responseData *responseData
+	}
+)
+
+func (r *loggingResponseWriter) Write(b []byte) (int, error) {
+	size, err := r.ResponseWriter.Write(b)
+	r.responseData.size += size
+	return size, err
+}
+
+func (r *loggingResponseWriter) WriteHeader(status int) {
+	r.ResponseWriter.WriteHeader(status)
+	r.responseData.status = status
+}
+
+func withLogging(sugar *zap.SugaredLogger) func(next http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			start := time.Now()
+			uri := r.RequestURI
+			method := r.Method
+
+			responseData := &responseData{
+				status: http.StatusOK,
+				size:   0,
+			}
+
+			lw := &loggingResponseWriter{ResponseWriter: w, responseData: responseData}
+			next.ServeHTTP(lw, r)
+
+			duration := time.Since(start)
+
+			sugar.Infow(
+				"request completed",
+				"uri", uri,
+				"method", method,
+				"duration", duration,
+				"status", responseData.status,
+				"size", responseData.size,
+			)
+		})
+	}
 }
