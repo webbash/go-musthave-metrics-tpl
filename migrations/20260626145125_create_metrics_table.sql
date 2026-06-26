6702604932c8ee6ee6aba9fcc89916a359df4358
Denis Lukhmanov <denis.lukhmanov@brainsome.com>
2026-06-26 18:15:08 +0300

    Iter 10: Add migration for metrics table

---
+++ migrations/20260626145125_create_metrics_table.sql
@@ -0,0 +1,13 @@
+-- +goose Up
+CREATE TABLE metrics
+(
+    id    TEXT PRIMARY KEY,
+    type  TEXT NOT NULL,
+    delta BIGINT,
+    value DOUBLE PRECISION
+);
+-- goose -dir migrations postgres \
+-- "postgres://metrics:metrics@localhost:5432/metrics?sslmode=disable" up
+
+-- +goose Down
+DROP TABLE IF EXISTS metrics;
