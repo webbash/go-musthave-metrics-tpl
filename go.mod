d1d7bcc1a7ade21207ff863607f8892303e2a562
Denis Lukhmanov <d.luhmanov@univertv.ru>
2026-07-04 08:49:58 +0300

    Iter 13: Add retry logic for PostgreSQL errors

--- go.mod
+++ go.mod
@@ -3,9 +3,9 @@ module github.com/webbash/go-musthave-metrics-tpl.git
 go 1.26.0
 
 require (
-	github.com/Masterminds/squirrel v1.5.4
 	github.com/caarlos0/env/v11 v11.4.1
 	github.com/go-chi/chi/v5 v5.2.5
+	github.com/jackc/pgerrcode v0.0.0-20250907135507-afb5586c32a6
 	github.com/jackc/pgx/v5 v5.10.0
 	github.com/pressly/goose/v3 v3.27.1
 	github.com/stretchr/testify v1.11.1
@@ -17,8 +17,6 @@ require (
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
 	github.com/jackc/puddle/v2 v2.2.2 // indirect
-	github.com/lann/builder v0.0.0-20180802200727-47ae307949d0 // indirect
-	github.com/lann/ps v0.0.0-20150810152359-62de8c46ede0 // indirect
 	github.com/mfridman/interpolate v0.0.2 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/rogpeppe/go-internal v1.15.0 // indirect
---
+++ internal/retry/postgres.go
@@ -0,0 +1,133 @@
+package retry
+
+import (
+	"context"
+	"database/sql"
+	"errors"
+	"fmt"
+
+	"github.com/jackc/pgerrcode"
+	"github.com/jackc/pgx/v5/pgconn"
+)
+
+package pgerrors
+
+import (
+"errors"
+
+"github.com/jackc/pgerrcode"
+"github.com/jackc/pgx/v5/pgconn"
+)
+
+// ErrorClassification тип для классификации ошибок
+type PGErrorClassification int
+
+const (
+	// NonRetriable - операцию не следует повторять
+	NonRetriable PGErrorClassification = iota
+
+	// Retriable - операцию можно повторить
+	Retriable
+)
+
+// PostgresErrorClassifier классификатор ошибок PostgreSQL
+type PostgresErrorClassifier struct{}
+
+func NewPostgresErrorClassifier() *PostgresErrorClassifier {
+	return &PostgresErrorClassifier{}
+}
+
+// Classify классифицирует ошибку и возвращает PGErrorClassification
+func (c *PostgresErrorClassifier) Classify(err error) PGErrorClassification {
+	if err == nil {
+		return NonRetriable
+	}
+
+	// Проверяем и конвертируем в pgconn.PgError, если это возможно
+	var pgErr *pgconn.PgError
+	if errors.As(err, &pgErr) {
+		return СlassifyPgError(pgErr)
+	}
+
+	// По умолчанию считаем ошибку неповторяемой
+	return NonRetriable
+}
+
+func СlassifyPgError(pgErr *pgconn.PgError) PGErrorClassification {
+	// Коды ошибок PostgreSQL: https://www.postgresql.org/docs/current/errcodes-appendix.html
+
+	switch pgErr.Code {
+	// Класс 08 - Ошибки соединения
+	case pgerrcode.ConnectionException,
+		pgerrcode.ConnectionDoesNotExist,
+		pgerrcode.ConnectionFailure:
+		return Retriable
+
+	// Класс 40 - Откат транзакции
+	case pgerrcode.TransactionRollback, // 40000
+		pgerrcode.SerializationFailure, // 40001
+		pgerrcode.DeadlockDetected:     // 40P01
+		return Retriable
+
+	// Класс 57 - Ошибка оператора
+	case pgerrcode.CannotConnectNow: // 57P03
+		return Retriable
+	}
+
+	// Можно добавить более конкретные проверки с использованием констант pgerrcode
+	switch pgErr.Code {
+	// Класс 22 - Ошибки данных
+	case pgerrcode.DataException,
+		pgerrcode.NullValueNotAllowedDataException:
+		return NonRetriable
+
+	// Класс 23 - Нарушение ограничений целостности
+	case pgerrcode.IntegrityConstraintViolation,
+		pgerrcode.RestrictViolation,
+		pgerrcode.NotNullViolation,
+		pgerrcode.ForeignKeyViolation,
+		pgerrcode.UniqueViolation,
+		pgerrcode.CheckViolation:
+		return NonRetriable
+
+	// Класс 42 - Синтаксические ошибки
+	case pgerrcode.SyntaxErrorOrAccessRuleViolation,
+		pgerrcode.SyntaxError,
+		pgerrcode.UndefinedColumn,
+		pgerrcode.UndefinedTable,
+		pgerrcode.UndefinedFunction:
+		return NonRetriable
+	}
+
+	// По умолчанию считаем ошибку неповторяемой
+	return NonRetriable
+}
+
+func executeWithRetry(db *sql.DB) error {
+	const maxRetries = 3
+	var lastErr error
+
+	classifier := NewPostgresErrorClassifier()
+
+	for attempt := 0; attempt < maxRetries; attempt++ {
+		ctx := context.Background()
+
+		_, err := db.ExecContext(ctx, "полезный SQL запрос")
+		if err == nil {
+			return nil
+		}
+
+		// Определяем классификацию ошибки
+		classification := classifier.Classify(err)
+
+		if classification == NonRetriable {
+			// Нет смысла повторять, возвращаем ошибку
+			fmt.Printf("Непредвиденная ошибка: %v\n", err)
+			return err
+		}
+
+		// .... делаем что-то полезное
+	}
+
+	return fmt.Errorf("операция прервана после %d попыток: %w", maxRetries, lastErr)
+}
