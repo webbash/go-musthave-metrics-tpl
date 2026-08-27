b80b867402b21b5146100dd99eb0006ec2d47518
Denis Lukhmanov <denis.lukhmanov@brainsome.com>
2026-08-27 18:54:35 +0300

    Iter 20: Edit list of analysers - add some from standart passes

--- cmd/staticlint/main.go
+++ cmd/staticlint/main.go
@@ -5,10 +5,16 @@ import (
 	"github.com/timakin/bodyclose/passes/bodyclose"
 	"golang.org/x/tools/go/analysis"
 	"golang.org/x/tools/go/analysis/multichecker"
+	"golang.org/x/tools/go/analysis/passes/errorsas"
+	"golang.org/x/tools/go/analysis/passes/httpresponse"
+	"golang.org/x/tools/go/analysis/passes/lostcancel"
+	"golang.org/x/tools/go/analysis/passes/nilfunc"
 	"golang.org/x/tools/go/analysis/passes/printf"
 	"golang.org/x/tools/go/analysis/passes/shadow"
-	"golang.org/x/tools/go/analysis/passes/shift"
 	"golang.org/x/tools/go/analysis/passes/structtag"
+	"golang.org/x/tools/go/analysis/passes/unmarshal"
+	"golang.org/x/tools/go/analysis/passes/unreachable"
+	"golang.org/x/tools/go/analysis/passes/unusedresult"
 	"honnef.co/go/tools/staticcheck"
 	"honnef.co/go/tools/stylecheck/st1000"
 )
@@ -16,10 +22,20 @@ import (
 func main() {
 	analyzers := []*analysis.Analyzer{
 		ErrOsExitAnalyzer,
+
+		// Passes analyzers.
 		printf.Analyzer,
 		shadow.Analyzer,
-		shift.Analyzer,
 		structtag.Analyzer,
+		errorsas.Analyzer,
+		httpresponse.Analyzer,
+		lostcancel.Analyzer,
+		nilfunc.Analyzer,
+		unmarshal.Analyzer,
+		unreachable.Analyzer,
+		unusedresult.Analyzer,
+
+		// Custom analyzers.
 		errcheck.Analyzer,
 		bodyclose.Analyzer,
 	}
