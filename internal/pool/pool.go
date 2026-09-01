0c95c46b19993bddfe387fbe8f9a4d7cafd07f81
Denis Lukhmanov <denis.lukhmanov@brainsome.com>
2026-09-01 18:36:01 +0300

    Iter 22: Add pool for resettable objects, add unit tests, add tasks from theory

---
+++ internal/pool/pool.go
@@ -0,0 +1,39 @@
+package pool
+
+import "sync"
+
+type Resettable interface {
+	Reset()
+}
+
+type Pool[T Resettable] struct {
+	objects []T
+	mu      sync.Mutex
+}
+
+func New[T Resettable]() *Pool[T] {
+	return &Pool[T]{}
+}
+
+func (p *Pool[T]) Put(obj T) {
+	p.mu.Lock()
+	defer p.mu.Unlock()
+
+	obj.Reset()
+	p.objects = append(p.objects, obj)
+}
+
+func (p *Pool[T]) Get() T {
+	p.mu.Lock()
+	defer p.mu.Unlock()
+
+	if len(p.objects) == 0 {
+		var zero T
+		return zero
+	}
+
+	obj := p.objects[len(p.objects)-1]
+	p.objects = p.objects[:len(p.objects)-1]
+
+	return obj
+}
